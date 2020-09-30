be56e5aa064f68b48ca31b3a6fcad71662a4d208
olekmali <olekmali@ieee.org>
2020-09-30 01:03:31 -0500

    Cleanup and simplified the very first OOP example - digital IO with LEDs and Keys

--- 01_oop_digital_io/c_digital_io/main_digital_io_c.c
+++ 01_oop_digital_io/c_digital_io/main_digital_io_c.c
@@ -1,34 +1,15 @@
 //* Simple Digital IO test set in plain C *
 #include "bios_keysc.h"
 #include "bios_ledsc.h"
-
 #include <stdint.h>
 
 # define F_CPU 16000000UL
 #include <util/delay.h>
 
 
-uint8_t choose_on_power_on(uint8_t wait) {
-    uint8_t     result=0;
-    do {
-        result = keys_get();
-    } while ( wait && (0==result) );
-
-    if (0!=result) {
-        uint8_t waitst=0;
-        while ( waitst<64 ) {
-            if ( 0 != keys_get() ) waitst=0;
-            else waitst++;
-        }
-    }
-    return(result);
-}
-
-
-
-void main_toggle1() {
+void main_toggle() {
     uint8_t  state = 0;
-    
+
     // MAIN SUPERLOOP
     while(1)
     {
@@ -47,11 +28,11 @@ void main_toggle1() {
 
 
 
-void main_menu1() {
+void main_menu() {
     uint8_t  state = 0;
     uint8_t  mode  = 1;
     uint8_t  prev  = 0;
-    
+
     // MAIN SUPERLOOP
     while(1)
     {
@@ -65,7 +46,7 @@ void main_menu1() {
 
         if (mode!=prev) {
             leds_set( leds_get() ^ mode ^ prev );
-            prev = mode;            
+            prev = mode;
         }
 
         // let's do at least lousy debouncing
@@ -74,40 +55,9 @@ void main_menu1() {
 }
 
 
-void main_blank1() {
-    while (1) ;
-}
-
-
-void main_blank2() {
-    while (1) ;
-}
-
-
 int main(void)
 {
-    // INITIALIZATION
-    leds_init();
-    keys_init();
-
-    switch ( choose_on_power_on(1) ) {
-    case B_K4 :
-//      leds_set(B_L5);
-        main_toggle1();
-        break;
-    case B_K5 :
-//      leds_set(B_L5|B_L6);
-        main_menu1();
-        break;
-    case B_K6 :
-//      leds_set(B_L5|B_L7);
-        main_blank1();
-        break;
-    case B_K7 : 
-//      leds_set(B_L5|B_L6|B_L7);
-        main_blank2();
-        break;
-    }
-
+    main_toggle();
+    // main_menu();
     return(0);
 }
